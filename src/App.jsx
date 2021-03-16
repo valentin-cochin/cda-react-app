0551784124e81a999a00e3bab516ee76dd9a6dd7
valentin-cochin <cochin.valentin@hotmail.fr>
2021-03-16 12:00:26 +0100

    Props + State

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import Hello from './components/Hello';
 
 class App extends React.Component {
 
+  state = {
+    login : 'Jean'
+  }
 
   render() {
     return (
       <div>
        <Hello login='Toto'/>
-       <Hello login='Chips'/>
+       <Hello login={this.state.login}/>
       </div>
     )
   }
