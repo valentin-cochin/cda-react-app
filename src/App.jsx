3e7c2bb515c4a43727e08c98bfb714f06a3b4ed2
valentin-cochin <cochin.valentin@hotmail.fr>
2021-03-16 11:33:36 +0100

    Fix component

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Hello from './components/Hello';
+
+
+class App extends React.Component {
+
+
+  render() {
+    return (
+      <div>
+       <Hello/>
+      </div>
+    )
+  }
+}
+
+export default App
